40d48bf5d8a434945fc500618308eed59f446200
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-05-20 23:17:29 +0300

    Fixing padding

--- libs/abstract-evm/src/lib/abstract-evm.ts
+++ libs/abstract-evm/src/lib/abstract-evm.ts
@@ -2,33 +2,33 @@ import { CoreNetworkAbstraction, AddressBalance, Transaction } from '@sfd/abstra
 import { networks } from '@sfd/crypto-assets';
 
 export abstract class EvmAbstraction extends CoreNetworkAbstraction {
-    protected rpcUrl: string;
+  protected rpcUrl: string;
 
-    constructor(rpcUrl: string) {
-        super();
-        this.rpcUrl = rpcUrl;
-    }
+  constructor(rpcUrl: string) {
+    super();
+    this.rpcUrl = rpcUrl;
+  }
 
-    async getAddressBalance(address: string, networkId: string): Promise<AddressBalance> {
-        // Implementation for fetching native balance using RPC
-        const network = networks[networkId];
-        return {
-            address,
-            nativeBalance: '0',
-            networkId,
-            networkName: network.name,
-            balances: [
-                {
-                    asset: 'ETH',
-                    amount: '0'
-                }
-            ]
-        };
-    }
+  async getAddressBalance(address: string, networkId: string): Promise<AddressBalance> {
+    // Implementation for fetching native balance using RPC
+    const network = networks[networkId];
+    return {
+      address,
+      nativeBalance: '0',
+      networkId,
+      networkName: network.name,
+      balances: [
+        {
+          asset: 'ETH',
+          amount: '0'
+        }
+      ]
+    };
+  }
 
-    async getTransactionHistory(address: string, networkId: string): Promise<Transaction[]> {
-        // Implementation for fetching transaction history using RPC
-        console.log(`Fetching transaction history for ${address} on ${networkId}`);
-        return [];
-    }
+  async getTransactionHistory(address: string, networkId: string): Promise<Transaction[]> {
+    // Implementation for fetching transaction history using RPC
+    console.log(`Fetching transaction history for ${address} on ${networkId}`);
+    return [];
+  }
 }
