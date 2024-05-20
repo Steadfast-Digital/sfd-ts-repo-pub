53941694d62984ab43c49051dcc0092b6a737c78
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-05-20 21:49:40 +0300

    Adding core abstraction

--- libs/abstract-core/src/lib/abstract-core.ts
+++ libs/abstract-core/src/lib/abstract-core.ts
@@ -1,3 +1,20 @@
-export function abstractCore(): string {
-  return 'abstract-core';
+export interface AddressBalance {
+  address: string;
+  nativeBalance: string;
+  balances: [
+    {
+      asset: string;
+      amount: string;
+    }
+  ];
+}
+
+export interface Transaction {
+  hash: string;
+  // Define transaction structure
+}
+
+export abstract class CoreNetworkAbstraction {
+  abstract getAddressBalance(address: string): Promise<AddressBalance>;
+  abstract getTransactionHistory(address: string): Promise<Transaction[]>;
 }
