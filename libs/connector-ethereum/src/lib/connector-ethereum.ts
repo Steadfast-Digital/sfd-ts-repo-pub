fbb0b1dfa246e66efc7c2213fa29221814d35e1b
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-05-23 16:03:14 +0300

    Adjusting ethereum connector

--- libs/connector-ethereum/src/lib/connector-ethereum.ts
+++ libs/connector-ethereum/src/lib/connector-ethereum.ts
@@ -1,9 +1,8 @@
 import { EvmAbstraction } from '@steadfastdigital/abstract-evm';
 
 export class EthereumConnector extends EvmAbstraction {
-  constructor() {
-    super('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+  constructor(networkId: string) {
+    super(networkId);
   }
-
   // You can override methods here if Ethereum specific logic is needed
 }
