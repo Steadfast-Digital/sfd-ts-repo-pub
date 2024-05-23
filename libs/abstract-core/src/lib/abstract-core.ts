cb46d1f05781dd5bc1cbc590ee534ab4c738275c
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-05-23 15:35:40 +0300

    Update to abstract core class and types

--- libs/abstract-core/src/lib/abstract-core.ts
+++ libs/abstract-core/src/lib/abstract-core.ts
@@ -1,23 +1,50 @@
-export interface AddressBalance {
-  address: string;
-  nativeBalance: string;
-  networkId: string;
-  networkName: string;
-  balances: [
-    {
-      asset: string;
-      amount: string;
-    }
-  ];
+import { NativeAsset, TokenAsset } from '@steadfastdigital/crypto-assets';
+
+export type NativeAssetBalance = {
+  asset: NativeAsset;
+  amount: string;
+  minBalance?: string | undefined;
 }
 
+export interface AddressBalance {
+  address: string;
+  native: NativeAssetBalance;
+  fees: NativeAssetBalance[];
+}
+
+export interface AssetBalance {
+  asset: NativeAsset | TokenAsset;
+  amount: string;
+  minBalance?: string | undefined;
+}
+export interface AddressBalances extends AddressBalance {
+  tokens: AssetBalance[];
+}
 export interface Transaction {
   hash: string;
   from: string;
-  // Define transaction structure
+  to: string;
+  value: string;
+  fee: {
+    asset: NativeAsset;
+    amount: string;
+  }
+  blockNumber: number;
+  timestamp: number;
+  status: 'confirmed' | 'pending' | 'failed';
+  asset: NativeAsset | TokenAsset;
+  nonce: number;
 }
 
 export abstract class CoreNetworkAbstraction {
-  abstract getAddressBalance(address: string, networkId: string): Promise<AddressBalance>;
-  abstract getTransactionHistory(address: string, networkId: string): Promise<Transaction[]>;
+  // create a constractor that takes in a networkId
+  private _networkId: string;
+  constructor(protected networkId: string) {
+    this._networkId = networkId;
+  }
+  abstract getAddressBalance(address: string): Promise<AddressBalance>;
+  abstract getAddressBalances(address: string): Promise<AddressBalances>;
+  abstract getAddressAssetBalance(address: string, assetId: string): Promise<AssetBalance>;
+  abstract getAddressAssetsBalances(address: string, assetIds: string[]): Promise<AssetBalance[]>;
+  abstract getTransactionHistory(address: string): Promise<Transaction[]>;
 }
