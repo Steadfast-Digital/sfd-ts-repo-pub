c36f1dfa264de6c9a1b7add103e36722e4f03609
Stultus Mundi <bmg.stultusmundi@gmail.com>
2024-06-05 17:19:55 +0300

    Adding new wrapper methods

--- libs/blockchain-factory/src/lib/factory-wraper.ts
+++ libs/blockchain-factory/src/lib/factory-wraper.ts
@@ -4,3 +4,23 @@ export async function getAddressBalance(networkId: string, address: string) {
   const blockchain = await BlockchainFactory.createBlockchain(networkId);
   return blockchain.getAddressBalance(address);
 }
+
+export async function getAddressBalances(networkId: string, address: string) {
+  const blockchain = await BlockchainFactory.createBlockchain(networkId);
+  return blockchain.getAddressBalances(address);
+}
+
+export async function getAddressAssetBalance(networkId: string, address: string, assetId: string) {
+  const blockchain = await BlockchainFactory.createBlockchain(networkId);
+  return blockchain.getAddressAssetBalance(address, assetId);
+}
+
+export async function getAddressAssetsBalances(networkId: string, address: string, assetIds: string[]) {
+  const blockchain = await BlockchainFactory.createBlockchain(networkId);
+  return blockchain.getAddressAssetsBalances(address, assetIds);
+}
+
+export async function getTransactionHistory(networkId: string, address: string) {
+  const blockchain = await BlockchainFactory.createBlockchain(networkId);
+  return blockchain.getTransactionHistory(address);
+}
